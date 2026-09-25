cab5d4c6bc8c6dbc8e3a221f7502c5166db426ea
wwavaleedasdfghjkl-spec <wwavaleedasdfghjkl@gmail.com>
2026-09-26 01:23:11 +0300

    Update toggle.tsx

--- artifacts/oxy-air-perfume/src/components/ui/toggle.tsx
+++ artifacts/oxy-air-perfume/src/components/ui/toggle.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import * as TogglePrimitive from '@radix-ui/react-toggle';
+//import * as TogglePrimitive from '@radix-ui/react-toggle';
 import { cn } from '@/lib/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
